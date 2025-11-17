ac2a7e1745d0cf66ec5402661b4ae03be64fbe91
grcsania <gracesania9@gmail.com>
2025-11-17 16:40:13 +0700

    init

--- database/migrations/2025_11_15_163857_add_status_to_todos_table.php
+++ database/migrations/2025_11_15_163857_add_status_to_todos_table.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -8,15 +7,11 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('todos', function (Blueprint $table) {
-            $table->text('note')->nullable()->after('title');
-        });
+        // DIBIARKAN KOSONG untuk melewati duplikasi kolom.
     }
 
     public function down(): void
     {
-        Schema::table('todos', function (Blueprint $table) {
-            $table->dropColumn('note');
-        });
+        // DIBIARKAN KOSONG
     }
 };
