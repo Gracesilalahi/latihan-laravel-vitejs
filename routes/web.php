07a2354a480a0ec875893ed878006ddd9704b9ef
grcsania <gracesania9@gmail.com>
2025-11-19 08:47:48 +0700

    Remove Jetstream & Fortify, clean project

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,17 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// Arahkan root "/" ke halaman login
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
+// Dashboard, hanya bisa diakses setelah login & email terverifikasi
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Group route untuk user yang sudah login
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
